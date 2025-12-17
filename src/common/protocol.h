4b643e653e685b9c2dd9c0b86d4517c26e4ce5b5
ddtd146 <dangtiendungko@gmail.com>
2025-12-17 06:26:56 +0000

    add msg type + payload struct

--- src/common/protocol.h
+++ src/common/protocol.h
@@ -25,27 +25,31 @@
 
 // --- MESSAGE TYPES ---
 // Auth
-#define MSG_REGISTER_REQ 1
-#define MSG_REGISTER_RES 2
-#define MSG_LOGIN_REQ 3
-#define MSG_LOGIN_RES 4
-#define MSG_LOGOUT_REQ 5
-#define MSG_LOGOUT_RES 6
-#define MSG_SEND_CLIENT_INFO_REQ 7
-#define MSG_SEND_CLIENT_INFO_RES 8
+#define MSG_REGISTER_REQ                1
+#define MSG_REGISTER_RES                2
+#define MSG_LOGIN_REQ                   3
+#define MSG_LOGIN_RES                   4
+#define MSG_LOGOUT_REQ                  5
+#define MSG_LOGOUT_RES                  6
+#define MSG_SEND_CLIENT_INFO_REQ        7
+#define MSG_SEND_CLIENT_INFO_RES        8
+
+// Client 
+#define MSG_REGISTER_P2P_POR_REQ        9
+#define MSG_REGISTER_P2P_POR_RES        10
 
 // File
-#define MSG_SHARE_FILE_REQ 9
-#define MSG_SHARE_FILE_RES 10
-#define MSG_UNSHARE_FILE_REQ 11
-#define MSG_UNSHARE_FILE_RES 12
-#define MSG_SEARCH_FILE_REQ 13
-#define MSG_SEARCH_FILE_RES 14
+#define MSG_SHARE_FILE_REQ              11
+#define MSG_SHARE_FILE_RES              12
+#define MSG_UNSHARE_FILE_REQ            13
+#define MSG_UNSHARE_FILE_RES            14
+#define MSG_SEARCH_FILE_REQ             15
+#define MSG_SEARCH_FILE_RES             16
 // Download
-#define MSG_P2P_DOWNLOAD_REQ 15
-#define MSG_P2P_DOWNLOAD_RES 16
-#define MSG_P2P_FILE_DATA 17
-#define MSG_P2P_ERROR 18
+#define MSG_P2P_DOWNLOAD_REQ            17
+#define MSG_P2P_DOWNLOAD_RES            18
+#define MSG_P2P_FILE_DATA               19
+#define MSG_P2P_ERROR                   20
 
 // --- STATUS CODES ---
 // Generic codes
@@ -62,6 +66,7 @@
 // File related code
 #define STATUS_ERR_FILE_ALREADY_SHARED 15
 
+#pragma pack(push, 1)
 // --- PACKET STRUCTURE ---
 // MESSAGE = [HEADER][PAYLOAD]
 // -- Header struct --
@@ -105,7 +110,17 @@ typedef struct {
     uint8_t status;
 } logout_res_t;
 
-// 2. File management
+// 2. File related
+
+typedef struct {
+    uint32_t client_id;
+    uint16_t p2p_port;
+} register_p2p_port_req_t;
+
+typedef struct {
+    uint8_t status; 
+    
+} register_p2p_port_res_t;
 
 typedef struct {
     uint32_t client_id;
@@ -115,4 +130,7 @@ typedef struct {
 typedef struct {
     uint8_t status; 
 } share_file_res_t;
+
+#pragma pack(pop);
+
 #endif
